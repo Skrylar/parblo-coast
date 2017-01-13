0649bac788354c52bd0f49ab9100fcdf13fde1f6
Joshua Cearley <joshua.cearley@gmail.com>
2017-01-13 06:10:31 -0600

    Added a GPLv2 license.

--- parblo-coast10.c
+++ parblo-coast10.c
@@ -1,3 +1,33 @@
+/*
+ *  Parblo Coast10 tablet monitor support
+ *
+ *  Copyright (c) 2017 Joshua "Skrylar" Cearley <joshua.cearley@gmail.com>
+ *
+ * Xing Wei's original Hanwang tablet driver was used as the
+ * structural template for this driver, although the actual reverse
+ * engineering and implementation was done by me.
+ *
+ * Available from
+ * http://lxr.free-electrons.com/source/drivers/input/tablet/hanwang.c
+ *
+ */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
 
 #include <linux/version.h>
 #include <linux/module.h>
@@ -242,6 +272,5 @@ static struct usb_driver parblo_driver = {
 module_usb_driver(parblo_driver);
 
 MODULE_AUTHOR("Joshua Cearley <joshua.cearley@gmail.com>");
-/*MODULE_AUTHOR("Xing Wei <weixing@hanwang.com.cn>")*/
 MODULE_LICENSE("GPL");
 MODULE_VERSION("1");
